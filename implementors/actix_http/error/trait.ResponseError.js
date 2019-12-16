f34faeef013cfb595e933d7494d960cb4323c09b
Travis CI User <travis@example.org>
2019-12-16 02:21:22 +0000

    Documentation upload

---
+++ implementors/actix_http/error/trait.ResponseError.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["actix"] = [{text:"impl ResponseError for <a class=\"enum\" href=\"actix/actors/resolver/enum.ResolverError.html\" title=\"enum actix::actors::resolver::ResolverError\">ResolverError</a>",synthetic:false,types:["actix::actors::resolver::ResolverError"]},{text:"impl ResponseError for <a class=\"enum\" href=\"actix/enum.MailboxError.html\" title=\"enum actix::MailboxError\">MailboxError</a>",synthetic:false,types:["actix::address::MailboxError"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
---
+++ implementors/core/marker/trait.Copy.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["actix"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"actix/enum.ActorState.html\" title=\"enum actix::ActorState\">ActorState</a>",synthetic:false,types:["actix::actor::ActorState"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"actix/enum.Running.html\" title=\"enum actix::Running\">Running</a>",synthetic:false,types:["actix::actor::Running"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"actix/struct.SpawnHandle.html\" title=\"struct actix::SpawnHandle\">SpawnHandle</a>",synthetic:false,types:["actix::actor::SpawnHandle"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
